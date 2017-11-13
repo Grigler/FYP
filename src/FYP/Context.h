f1d32b27e33f376f1b07c97e23b306d4990380ab
Grigler <graham.rigler@gmail.com>
2017-11-13 12:44:11 +0000

    Fixed git case sensitivity

---
+++ src/FYP/Context.h
@@ -0,0 +1,26 @@
+#ifndef __FYP_CONTEXT__
+#define __FYP_CONTEXT__
+
+#include "clew/clew.h"
+
+namespace FYP
+{
+  class Context
+  {
+  public:
+    void Create(cl_device_type _devices);
+
+    cl_context *GetId() { return context; }
+
+  private:
+    cl_platform_id platform;
+
+    cl_device_id devices[10];
+    cl_uint numDevices;
+    
+    cl_context *context;
+    cl_command_queue commandQueue[2];
+  };
+}
+
+#endif
